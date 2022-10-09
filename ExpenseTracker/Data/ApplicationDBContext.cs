97d4e06a9f8f3b0aeb16cf770038dc60c6520fa0
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-10-09 16:46:21 +0200

    Installed Entity Framework Tools

---
+++ ExpenseTracker/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using BorrowLanded.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BorrowLanded.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Item> Item { get; set; }
+    }
+}
