cd1701e5e6e1495e273c27106a9250e8ca38b65f
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-10-09 17:07:59 +0200

    Passing the database to the view

--- ExpenseTracker/Controllers/ItemController.cs
+++ ExpenseTracker/Controllers/ItemController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BorrowLanded.Data;
+using BorrowLanded.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace BorrowLanded.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly ApplicationDBContext _db;
+
+        public ItemController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Item> objList = _db.Item;
+            return View(objList);
         }
     }
 }
