44e782f86d78e5588340f2c374a6da81dabbb86c
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-10-10 01:39:08 +0200

    Created the expenses View Table

--- ExpenseTracker/Controllers/ExpensesController.cs
+++ ExpenseTracker/Controllers/ExpensesController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BorrowLanded.Data;
+using BorrowLanded.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace BorrowLanded.Controllers
 {
     public class ExpensesController : Controller
     {
+        private readonly ApplicationDBContext _db;
+
+        public ExpensesController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Expenses> objList = _db.Expenses;
+            return View(objList);
         }
+
     }
 }
