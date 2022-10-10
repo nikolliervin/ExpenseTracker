743ac4d093e2f5f6b1aa6080179fdc996dbaa1b6
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-10-10 02:08:20 +0200

    Create Expense Entry View

--- ExpenseTracker/Models/Expenses.cs
+++ ExpenseTracker/Models/Expenses.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BorrowLanded.Models
 {
@@ -7,8 +8,10 @@ namespace BorrowLanded.Models
         [Key]
         public int Id { get; set; }
 
+        [DisplayName("Expense Name")]
         public string ExpenseName { get; set; }
 
+        [DisplayName("Expense Type")]
         public string ExpenseType { get; set; }
 
 
